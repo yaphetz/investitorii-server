c7a1905d49b678461673ae0dd7945ea916ed2868
Marius <Marius@DESKTOP-RPDOLTQ>
2022-09-09 02:50:59 +0300

    fixed events

--- functions/fb_events/order.js
+++ functions/fb_events/order.js
@@ -11,44 +11,25 @@ api.use(cors());
 const objectMap = (obj, fn) => Object.fromEntries(Object.entries(obj).map(([k, v], i) => [k, fn(v, k, i)]));
 
 api.post("/order", async (req, res) => {
-  let body = req.body;
-  let user_data = body.user_data;
-  let custom_data = body.custom_data;
-  let event_name = body.event_name;
-  let event_id = body.event_id;
-  let test_event_code = body.test_event_code;
-
-  user_data = objectMap(user_data, (data) => crypto.createHash("sha256").update(data).digest("hex"));
-
-  let eventData = {
-    data: [
-      {
-        event_name: event_name,
-        event_time: Math.floor(new Date().getTime() / 1000),
-        event_id: event_id,
-        user_data: user_data,
-        custom_data: custom_data,
-      },
-    ],
-    test_event_code: test_event_code,
-  };
-
-  let event = lodash.cloneDeep(eventData);
+  let event = lodash.cloneDeep(req.body);
+  event.data[0].event_time = Math.floor(new Date().getTime() / 1000);
+  event.data[0].user_data = objectMap(event.data[0].user_data, (data) => crypto.createHash("sha256").update(data).digest("hex"));
+  console.log(JSON.stringify(event))
 
   Promise.all([
-    fetch(
-      "https://graph.facebook.com/v12.0/195324979372461/events?access_token=EAADbwyncwZAEBALnH3ezPnXNuZCqWoDSEJUAFyOeKZCpelVVgXnIy5vhO7GWJBdTWxfi0gDmzM3JEZCVaSV5K1ZAJL9PCUgU7UfpOzQM4oFjFTKMyZCvfoclWplvZCWEDAH1obwUZBeTxN1kRNsET2xpn4Q8Klz0ZCNQFEggwUBIEDR2fZCbT6xDkZAH9ak0d9AqA8ZD",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(event),
-      }
-    ).then((resp) => resp.json().then((result) => console.log(result))),
+    // fetch(
+    //   "https://graph.facebook.com/v12.0/195324979372461/events?access_token=EAADbwyncwZAEBALnH3ezPnXNuZCqWoDSEJUAFyOeKZCpelVVgXnIy5vhO7GWJBdTWxfi0gDmzM3JEZCVaSV5K1ZAJL9PCUgU7UfpOzQM4oFjFTKMyZCvfoclWplvZCWEDAH1obwUZBeTxN1kRNsET2xpn4Q8Klz0ZCNQFEggwUBIEDR2fZCbT6xDkZAH9ak0d9AqA8ZD",
+    //   {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(event),
+    //   }
+    // ).then((resp) => resp.json().then((result) => console.log(result))),
 
     fetch(
-      "https://graph.facebook.com/v12.0/382464103878420/events?access_token=EAARp6yc6RlgBAMZAhWD4vp2cDWwL3Ca1WoOssuw5cOnUlZAuovtGJWRDEkjkNlRwDcAXaA8U1WuxWmfnZAlCNMYauyomKCAwPKiZBmEoZCy60u8IAW5wx8S9ZCXVACBvBqZCWGZBe4cvBZBHMLhw4ebVtiXZBV6pu9Cs3TWuR5HWOiKrIMWO63lgko",
+      "https://graph.facebook.com/v12.0/382464103878420/events?access_token=EAARp6yc6RlgBAHgW9Y0FFNOYO5p0xgZBSvNKmBzvOi9R5IADDpGGmxjbJViZAwPLHtIby6dFMo5xs0b7V5LHnNuOEzKyMHUcTYYmbziR8yLqDV2KQ43xjZCM7f4mETm80MOW3D7b09RjdrzwMJihHVPZA8821lTRrTSNbZBMzOGFuhIMc86sdYKkwT1BhJwoZD",
       {
         method: "POST",
         headers: {
