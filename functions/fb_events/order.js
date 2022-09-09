11643f2da719d422c4df12ebe250c57abfacc23b
Marius <Marius@DESKTOP-RPDOLTQ>
2022-09-09 11:51:38 +0300

    added both pixels

--- functions/fb_events/order.js
+++ functions/fb_events/order.js
@@ -17,16 +17,16 @@ api.post("/order", async (req, res) => {
   console.log(JSON.stringify(event))
 
   Promise.all([
-    // fetch(
-    //   "https://graph.facebook.com/v12.0/195324979372461/events?access_token=EAADbwyncwZAEBALnH3ezPnXNuZCqWoDSEJUAFyOeKZCpelVVgXnIy5vhO7GWJBdTWxfi0gDmzM3JEZCVaSV5K1ZAJL9PCUgU7UfpOzQM4oFjFTKMyZCvfoclWplvZCWEDAH1obwUZBeTxN1kRNsET2xpn4Q8Klz0ZCNQFEggwUBIEDR2fZCbT6xDkZAH9ak0d9AqA8ZD",
-    //   {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify(event),
-    //   }
-    // ).then((resp) => resp.json().then((result) => console.log(result))),
+    fetch(
+        "https://graph.facebook.com/v12.0/195324979372461/events?access_token=EAADbwyncwZAEBAI9lZBL5AJ2WfvjtfVr57JiRmHETZA97sgdljiUmoJMHVkqOhhevFltZADV39skFToBWig8cKI4IsGe6eLWw8h7fnoPdCDMLrTUoEYWFYFZA2SdVLuBJhBA1mE1yrVpJ1iCdg15T8ZCjWxihohBN8fHuNjlFWmwhZCog5UozN2AuQjozmpPKUZD",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(event),
+        }
+      ).then((resp) => resp.text().then((result) => console.log(result))),
 
     fetch(
       "https://graph.facebook.com/v12.0/382464103878420/events?access_token=EAARp6yc6RlgBAAIvSr6QDO6peyVacmmZAyZAgdjo1nSW8ZCvFxW04zSnJ4pZB04BIybrsUZA4hqlDZAheIr37HNJlCB0mD90XdGhhP9UF4vaq9YzSJA1sbN0zjtfczkvjZCsA6M8qiK9OqtBnd4ZCLc6E3p97ZByuLPPZChe65kGVPGMIieBdLFPaychZAUDFpZAl9AZD",
