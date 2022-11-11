ccbdd3330c4548a083a00b40903447d75deb0335
Marius <Marius@DESKTOP-RPDOLTQ>
2022-11-11 13:48:42 +0200

    added formular trimis

--- functions/fb_events/webinar-harta.js
+++ functions/fb_events/webinar-harta.js
@@ -64,4 +64,48 @@ api.post("/webinar-harta-evergreen", async (req, res) => {
 
 });
 
+function mapTypeform(response) {
+  return response.answers.map(answerElement=> {
+    let question = response.definition.fields.find(question=> question.id === answerElement.field.id)
+    let answer;
+    if(answerElement.text)
+        answer = answerElement.text;
+    if(answerElement.choice)
+        answer = answerElement.choice.label;
+    if(answerElement.email)
+        answer = answerElement.email;
+    if(answerElement.phone_number)
+        answer = answerElement.phone_number;
+    return {question: question.title, answer: answer}
+})
+}
+
+api.post("/formular-trimis", async (req, res) => {
+  console.log(req.body)
+  let request = mapTypeform(req.body.form_response);
+  let eventTitle = 'Inscriere Webinar Typefrom Evergreen';
+  let eventID = Buffer.from(object.email + object.firstName).toString('base64');
+
+  let event = {
+    event: eventTitle,
+    event_data: { eventID: object.form_response.answers[4].email },
+    user_data: {
+      em: object.form_response.answers[4].email,
+      ph: object.form_response.answers[5].phone_number,
+      fn: object.form_response.answers[1].text,
+    },
+  };
+
+  try {
+    Promise.all([
+      fetch('https://ennp1tgfp7yaxlt.m.pipedream.net', { method: "POST", body: JSON.stringify(event) }).then(resp => resp.text()),
+      fetch('https://enkbo1a3hlbh21q.m.pipedream.net', { method: "POST", body: JSON.stringify(event) }).then(resp => resp.text()),
+    ]).then(res.send(`${eventTitle} triggered`));
+  } catch (error) {
+    res.send(`${eventTitle} ERROR `, error);
+  }
+
+
+});
+
 exports.pixel = functions.https.onRequest(api);
---
+++ functions/tools/test.js
@@ -0,0 +1,32 @@
+let params = new URLSearchParams(window.location.search);
+let utmSource = params.get("utm_source");
+let utmMedium = params.get("utm_medium");
+let utmTerm = params.get("utm_term");
+let utmContent = params.get("utm_content");
+let utmCampaign = params.get("utm_campaign");
+let fbp = null;
+let fbc = null;
+let ip = null;
+let agent = null;
+console.log(`utm_source: ${utmSource}`);
+console.log(`utm_medium: ${utmMedium}`);
+console.log(`utm_term: ${utmTerm}`);
+console.log(`utm_content: ${utmContent}`);
+console.log(`utm_campaign: ${utmCampaign}`);
+function openLinkWithQueryParams() {
+  Promise.all([cookieStore.get("_fbp"), cookieStore.get("_fbc")]).then((cookies) => {
+    fetch("https://legacy.investitoriiromania.ro/typeform/getUserDataForPixel.php")
+      .then((res) => res.text())
+      .then((x) => {
+        x = JSON.parse(x);
+        ip = x.ip;
+        agent = x.agent;
+        cookies[0] ? (fbp = cookies[0].value) : null;
+        cookies[1] ? (fbc = cookies[1].value) : null;
+        window.open(
+          `https://investitorii.pro.typeform.com/VIPform#fbp=${fbp}&fbc=${fbc}&ip=${ip}&agent=${agent}&utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_term=${utmTerm}&utm_content=${utmContent}`,
+          `_self`
+        );
+      });
+  });
+}
