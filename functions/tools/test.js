e0da10b410ca8bb0373aae9fafbcb22760c45913
Marius <Marius@DESKTOP-RPDOLTQ>
2022-11-15 13:01:23 +0200

    formular trimis events search for fields dinamycally

--- functions/tools/test.js
+++ functions/tools/test.js
@@ -1,32 +1,29 @@
-let params = new URLSearchParams(window.location.search);
-let utmSource = params.get("utm_source");
-let utmMedium = params.get("utm_medium");
-let utmTerm = params.get("utm_term");
-let utmContent = params.get("utm_content");
-let utmCampaign = params.get("utm_campaign");
-let fbp = null;
-let fbc = null;
-let ip = null;
-let agent = null;
-console.log(`utm_source: ${utmSource}`);
-console.log(`utm_medium: ${utmMedium}`);
-console.log(`utm_term: ${utmTerm}`);
-console.log(`utm_content: ${utmContent}`);
-console.log(`utm_campaign: ${utmCampaign}`);
-function openLinkWithQueryParams() {
-  Promise.all([cookieStore.get("_fbp"), cookieStore.get("_fbc")]).then((cookies) => {
-    fetch("https://legacy.investitoriiromania.ro/typeform/getUserDataForPixel.php")
-      .then((res) => res.text())
-      .then((x) => {
-        x = JSON.parse(x);
-        ip = x.ip;
-        agent = x.agent;
-        cookies[0] ? (fbp = cookies[0].value) : null;
-        cookies[1] ? (fbc = cookies[1].value) : null;
-        window.open(
-          `https://investitorii.pro.typeform.com/VIPform#fbp=${fbp}&fbc=${fbc}&ip=${ip}&agent=${agent}&utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_term=${utmTerm}&utm_content=${utmContent}`,
-          `_self`
-        );
-      });
-  });
+function getCookies() {
+  const REGEXP = /([\w\.]+)\s*=\s*(?:"((?:\\"|[^"])*)"|(.*?))\s*(?:[;,]|$)/g;
+  let cookies = {};
+  let match;
+  while ((match = REGEXP.exec(document.cookie)) !== null) {
+    let value = match[2] || match[3];
+    cookies[match[1]] = decodeURIComponent(value);
+  }
+  return cookies;
 }
+
+const urlParams = new URLSearchParams(window.location.search);
+let eventName = "Formular Trimis";
+let pixelInfo = JSON.parse(urlParams.get("pixel"));
+const eventID = window.btoa(pixelInfo.email.split("@")[0] + pixelInfo.name);
+
+const interval = setInterval(() => {
+  if (getCookies()._fbp) {
+    
+    clearInterval(interval);
+    fbq("trackCustom", eventName, {}, { eventID: eventID });
+    if (document.referrer.includes("youcanbook")) {
+      const callEventName = "Book a call";
+      fbq("trackCustom", callEventName, {}, { eventID: eventID });
+    }
+  }
+}, 100);
+
+
